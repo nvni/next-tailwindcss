15dba698d7d20ec8821571adab5a2dc9ce3ae693
npv2k1 <npv2k1@gmail.com>
2021-09-18 22:35:40 +0700

    add components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header';
 export default function Home() {
   return (
     <div className="bg-gray-900">
+      <Header/>
       <h1 className="text-9xl text-white">Hello</h1>
     </div>
   );
